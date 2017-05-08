02fda49a4c1cc61c419f089864996f40df44107e
sahas.nanabala <sahas.nanabala@kony.com>
2017-05-08 19:08:44 +0530

    Angular refactoring + Adding shortlisted,recent job items:1

--- app/src/app.js
+++ app/src/app.js
@@ -1,16 +1,26 @@
 (function(){
   "use strict";
   var JobMineApp = angular.module('JobmineApp',['ui.router'])
-  .controller('JobViewController', JobViewController);
+  .controller('JobViewController', JobViewController)
+  .service('ShortlistJobService', ShortlistJobService);
 
   JobMineApp.config(StateConfig);
-
-  StateConfig.$inject=['$stateProvider','$urlRouterProvider'];
-  function StateConfig($stateProvider, $urlRouterProvider){
+  JobMineApp.component('jobTemplate', {
+    templateUrl:'templates/job_view.template.html',
+    bindings:{
+      templateTitle:'<',
+      jobDetails:'<',
+      shortlistClick:'&'
+    },
+    controller:
+  });
+  StateConfig.$inject=['$stateProvider'];
+  function StateConfig($stateProvider){
     // console.log("Inside stateConfig..");
-    $urlRouterProvider.otherwise('/');
+    //$urlRouterProvider.otherwise('/');
     $stateProvider
-    .state('state1', {
+    .state({
+      name:'state1',
       url: '/',
       templateUrl: 'templates/job_view.template.html',
       controller: 'JobViewController as ctr1',
@@ -28,30 +38,92 @@
      });
   }
 
-  JobViewController.$inject = ['jobPromise']
-  function JobViewController(jobPromise){
+  JobViewController.$inject = ['jobPromise','ShortlistJobService']
+  function JobViewController(jobPromise, ShortlistJobService){
     // console.log("Inside controller..")
     var ctr1 = this;
     // console.log("JobPromise:" + jobPromise);
       var jobDetailJson = jobPromise.data;
-      var jsonKeys = Object.keys(jobDetailJson);
-      var jsonModObj = [];
 
-      for(var key in jsonKeys){
-        var tempArr = jobDetailJson[key];
-        // console.log(tempArr);
-
-        for(var ind=0;ind < tempArr.length; ind++){
-          jsonModObj.push(tempArr[ind]);
-        }
-      }
       // console.log(jsonModObj);
-      ctr1.jobDetailJson = jsonModObj;
+      ctr1.fullJobDetailsJson= ShortlistJobService.getAndSetFullJobDetails(jobPromise.data);;
+      ctr1.shortlistedJobsJson= ShortlistJobService.getshortlistedJobsJson();
+      ctr1.lastVisitedJobsJson= ShortlistJobService.getlastVisitedJobsJson();
+      ctr1.lastWeekJobsJson= ShortlistJobService.getlastWeekJobsJson();
+      ctr1.jobDetailJson = ShortlistJobService.getjobDetailJson();
+
+      ctr1.shortListThisJob = function(jobId){
+        ShortlistJobService.shortListThisJob(ctr1.jobDetailJson, ctr1.shortlistedJobsJson, jobId);
+      }
+      ctr1.remShortListJob = function(jobId){
+        ShortlistJobService.remShortListJob(ctr1.jobDetailJson, ctr1.shortlistedJobsJson, jobId);
+      }
+
 
   }
 
   function MainController(){
 
+  }
+
+
+  function ShortlistJobService(){
+    var service = this;
+    var fullJson = null;
+    var fullJobDetailsJson =null;
+    var shortlistedJobsJson = null;
+    var lastVisitedJobsJson = null;
+    var lastWeekJobsJson = null;
+    var jobDetailJson = null;
+
+
+    service.setAllJobJsons = function(completeJson){
+      var modJson = completeJson.slice();
+      fullJson = completeJson.slice();
+      shortlistedJobsJson= modJson.splice(0,5);
+      lastVisitedJobsJson= modJson.splice(0,5);
+      lastWeekJobsJson= modJson.splice(0,5);
+      jobDetailJson = modJson;
+    }
+
+    service.getshortlistedJobsJson = function(){
+      return shortlistedJobsJson;
+    }
+
+    service.getlastVisitedJobsJson = function(){
+      return lastVisitedJobsJson;
+    }
+
+    service.getlastWeekJobsJson = function(){
+      return lastWeekJobsJson;
+    }
+
+    service.getjobDetailJson = function(){
+      return jobDetailJson;
+    }
+
+    service.getAndSetFullJobDetails = function(jobDetailJson){
+      var jsonKeys = Object.keys(jobDetailJson);
+      var completeJson = [];
+
+      for(var key in jsonKeys){
+        var tempArr = jobDetailJson[key];
+        // console.log(tempArr);
+        count=100;
+        for(var ind=0;ind < tempArr.length; ind++){
+          tempArr[i].id= count;
+          completeJson.push(tempArr[ind]);
+          count++;
+        }
+      }
+      service.setAllJobJsons(completeJson);
+      return completeJson;
+    }
+
+
+    service.shortListThisJob(completeJson, shortlistjson, id){
+
+    }
   }
 
 //   JobMineApp.run(['PrintToConsole', function(PrintToConsole) {
